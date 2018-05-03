32592fa2cc9204201b5ad362cfadfac521b1060c
Paul Johnson <paulj2850@gmail.com>
2018-05-03 15:34:12 -0500

    added chai-http

--- test/test-things.js
+++ test/test-things.js
@@ -1,51 +1,22 @@
 
+'use strict';
+
 const chai = require('chai');
-// const chaiHttp = require('chai-http');
+const chaiHttp = require('chai-http');
+const app = require('../server.js');
 
-// Import server.js and use destructuring assignment to create variables for
-// server.app, server.runServer, and server.closeServer
-const {app, runServer, closeServer} = require('../server');
-const expect = require('chai').expect;
+const expect = chai.expect;
 
-// chai.use(chaiHttp);
+chai.use(chaiHttp);
 
-
-
-// function isEqual (a, b) {
-// 	return a === b
-// }
-
-// // "describe" is used to declare the entity to be tested, and a callback function that sets up the tests
-// describe('isEqual', function() {
-//     // indicate the behavior to be tested
-//     // typically the "it" statement will also include a callback that provides the test
-//     it('should give right answers for equal and unequal inputs',  function(){
-//       expect(isEqual(1, 1)).to.be.true;
-//     });
-//   }
-// );
-
-
-
-describe('Connecting to root', function() {
-
-  before(function() {
-    return runServer();
-  });
-
-  after(function() {
-    return closeServer();
-  });
-
-  it('should connect to root and get a 200 status code and html', function() {
-	return chai.request(app)
-      .get('/public')
-	  .then(function(res) {
-	  		res.should.have.status(200);
-	        // expect(res).to.have.status(200);
-	        // expect(res.body).to.be.a('array');
-	        // expect(res.body.length).to.be.above(0);
-	       
+describe('Connect to html', function () {
+  it('should connect to root', function () {
+    return chai.request(app)
+      .get('/')
+      .then(function (res) {
+        expect(res).to.have.status(200);
       });
   });
 });
+
+
